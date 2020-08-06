a7261b54b767ecd793a9c2c84680542c8e454ac2
Victor Bonnier <Vikkear@hotmail.com>
2020-08-06 16:13:48 +0200

    updated server

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,17 +1,13 @@
 const Sequelize = require("sequelize");
+const config = require("./config.json");
 
 const db = {};
 const sequelize =
   process.env.NODE_ENV === "prod"
-    ? new Sequelize(
-        "d7j8b0hcm8nnva",
-        "nupslciiqxmqqi",
-        "38d8dfbb4eb2dfebf0a85eba1a3f075cae1993beed19444be741d46618fd89a4",
-        {
-          host: "ec2-54-247-94-127.eu-west-1.compute.amazonaws.com",
-          dialect: "postgres",
-        }
-      )
+    ? new Sequelize(config.database, config.username, config.password, {
+        host: config.host,
+        dialect: config.dialect,
+      })
     : new Sequelize("selfimprovement", "root", "1234", {
         host: "localhost",
         dialect: "mysql",
