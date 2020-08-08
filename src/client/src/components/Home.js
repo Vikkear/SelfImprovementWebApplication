d0cc36c15eb64b9134e756c92fe0eb48c146f84a
Victor Bonnier <Vikkear@hotmail.com>
2020-08-08 14:44:37 +0200

    Reworked home menu

--- src/client/src/components/Home.js
+++ src/client/src/components/Home.js
@@ -14,18 +14,36 @@ const Home = () => {
     window.location.replace("/todo");
   };
 
+  const login = () => {
+    window.location.replace("/login");
+  };
+
+  const isLoggedIn = localStorage.getItem("username") ? true : false;
+
   return (
     <div className="home">
-      <h1>Dashboard </h1>
-      <div className="navigationBtn" onClick={tracker}>
-        Tracker <br /> Tracks all of your entries
-      </div>
-      <div className="navigationBtn" onClick={quests}>
-        Quests (WIP) <br /> Add/Show Quests
-      </div>
-      <div className="navigationBtn" onClick={todo}>
-        Todo <br /> Todo list
-      </div>
+      {isLoggedIn ? (
+        <div className="loggedIn">
+          <h1>{localStorage.getItem("username")}'s Dashboard </h1>
+
+          <div className="navigationBtn" onClick={tracker}>
+            Tracker <br /> Tracks all of your entries
+          </div>
+          <div className="navigationBtn" onClick={quests}>
+            Quests (WIP) <br /> Add/Show Quests
+          </div>
+          <div className="navigationBtn" onClick={todo}>
+            Todo <br /> Todo list
+          </div>
+        </div>
+      ) : (
+        <div className="notLoggedIn">
+          <h1>Welcome!</h1>
+          <div className="navigationBtn" onClick={tracker}>
+            Login <br />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
